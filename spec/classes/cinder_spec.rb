f638b30287ae89589ae3ff358dc781a4aa97eb4b
Bogdan Dobrelya <bogdando@mail.ru>
2013-12-05 14:00:10 +0200

    Add logging related tests for main class
    Change-Id: I0e2cdefe2fc240e4eba090a5980323a302f0b431
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- spec/classes/cinder_spec.rb
+++ spec/classes/cinder_spec.rb
@@ -62,6 +62,15 @@ describe 'cinder' do
       )
     end
 
+    #FIXME : according to logging.pp should be absent for defaults!
+    it 'should not contain default config' do
+      pending('FIXME : according to logging.pp should be absent for defaults!')
+      should_not contain_cinder_config('DEFAULT/logging_context_format_string')
+      should_not contain_cinder_config('DEFAULT/logging_default_format_string')
+      should_not contain_cinder_config('DEFAULT/log_config_append')
+      should_not contain_cinder_config('DEFAULT/log_config')
+    end
+
     it { should contain_file('/etc/cinder/cinder.conf').with(
       :owner   => 'cinder',
       :group   => 'cinder',
@@ -141,10 +150,16 @@ describe 'cinder' do
 
   describe 'with syslog disabled' do
     let :params do
-      req_params
+      req_params.merge({
+        :log_dir => '/var/log/cinder',
+      })
     end
 
     it { should contain_cinder_config('DEFAULT/use_syslog').with_value(false) }
+    it { should contain_cinder_config('DEFAULT/log_dir').with_value('/var/log/cinder') }
+    it { should_not contain_cinder_config('DEFAULT/logdir') }
+    it { should_not contain_cinder_config('DEFAULT/logfile') }
+    it { should_not contain_cinder_config('DEFAULT/log_file') }
   end
 
   describe 'with syslog enabled' do
@@ -156,6 +171,7 @@ describe 'cinder' do
 
     it { should contain_cinder_config('DEFAULT/use_syslog').with_value(true) }
     it { should contain_cinder_config('DEFAULT/syslog_log_facility').with_value('LOG_USER') }
+    it { should_not contain_cinder_config('DEFAULT/use_stderr') }
   end
 
   describe 'with syslog enabled and custom settings' do
@@ -168,6 +184,47 @@ describe 'cinder' do
 
     it { should contain_cinder_config('DEFAULT/use_syslog').with_value(true) }
     it { should contain_cinder_config('DEFAULT/syslog_log_facility').with_value('LOG_LOCAL0') }
+  end
+
+  describe 'with custom context format string' do
+    let :params do
+      req_params.merge({
+        :logging_context_format_string   => '%(asctime)s %(levelname)s %(name)s [%(request_id)s %(user_id)s %(project_id)s] %(instance)s %(message)s',
+     })
+    end
+
+    it { should contain_cinder_config('DEFAULT/logging_context_format_string').with_value(
+      '%(asctime)s %(levelname)s %(name)s [%(request_id)s %(user_id)s %(project_id)s] %(instance)s %(message)s'
+    ) }
+  end
+
+  describe 'with custom default format string' do
+    let :params do
+      req_params.merge({
+        :logging_default_format_string  => '%(asctime)s %(levelname)s %(name)s [-] %(instance)s %(message)s',
+     })
+    end
+
+    it { should contain_cinder_config('DEFAULT/logging_default_format_string').with_value(
+      '%(asctime)s %(levelname)s %(name)s [-] %(instance)s %(message)s'
+    ) }
+  end
+
+  describe 'with custom log config template name' do
+    let :params do
+      req_params.merge({
+        :log_config  => 'logging_syslog.conf.erb',
+     })
+    end
+
+    it { should contain_file('/etc/cinder/logging.conf').with(
+     :owner   => 'cinder',
+     :group   => 'cinder',
+     :mode    => '0600',
+     :require => 'File[/etc/cinder/cinder.conf]'
+    ) }
+
+    it { should contain_cinder_config('DEFAULT/log_config_append').with_value('/etc/cinder/logging.conf') }
   end
 
 end
